d5609552595366a0c5ecd575706527b6fc1147fa
Aurelia <aurelia.foucher@outlook.com>
2023-05-15 16:33:27 +0200

    passage au mvc

--- Views/view_home.php
+++ Views/view_home.php
@@ -1,9 +1,35 @@
-<!-- MAIN -->
+<!DOCTYPE html>
+<html lang="fr">
 
-<div class="container">
-<main>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quizz Dev</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/app.js" defer></script>
+</head>
+
+<body>
+
+    <div class="container">
+
+
+        <!-- HEADER -->
+
+        <header>
+            <div>
+            </div>
+        </header>
+
+
+
+        <!-- MAIN -->
+
+        <main id="mainIndex">
             <h1>Welcome</h1>
 
+            <!-- comment mettre ça en hover -->
             <div> <a href="pageHtml.html">
                     <div class="categories"><img src="images/star.png" alt=""></div>
                     <p>HTML</p>
@@ -25,7 +51,36 @@
                     <p>PHP</p>
                 </a>
             </div>
-            <div>
-            </div>
+
+        <!-- DIV TRICHE -->
+            <div></div>
+            <div></div>
+
         </main>
-        </div>
+
+
+
+        <!-- FOOTER -->
+
+        <footer>
+            <div><a href="">
+                    <div class=""><img src="images/circle.png" alt=""></div>
+                    <p>Accueil</p>
+                </a>
+            </div>
+            <div>
+                <div class=""><img src="images/polygon.png" alt=""></div>
+                <p> </p>
+            </div>
+            <div><a href="">
+                    <div class=""><img src="images/star.png" alt=""></div>
+                    <p>Déconnexion</p>
+                </a>
+            </div>
+        </footer>
+
+    </div>
+
+</body>
+
+</html>
---
+++ Views/view_question.php
@@ -0,0 +1,46 @@
+<!-- QUESTIONS -->
+
+
+<div class="container">
+
+    <div class="questions">
+
+        <h1>HTML</h1>
+        <h2>Question 1</h2>
+        <p>Contenu de la question 1...</p>
+
+        <form class="choix">
+
+            <a href="#">
+                <label for="q1_choix1">
+                    <input type="button" id="q1_choix1" name="question1" value=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab quos ipsum vel nisi ex sed explicabo modi est quaerat velit aperiam aspernatur saepe ut, tenetur fuga et, atque cupiditate ratione nihil soluta quis unde.">
+                </label>
+            </a>
+
+
+            <a href="#">
+                <label for="q1_choix2">
+                    <input type="button" id="q1_choix2" name="question1" value="choix2">
+                </label>
+            </a>
+
+            <a href="#">
+                <label for="q1_choix3">
+                    <input type="button" id="q1_choix3" name="question1" value="choix3">
+                </label>
+            </a>
+
+            <a href="#">
+                <label for="q1_choix4">
+                    <input type="button" id="q1_choix4" name="question1" value="choix4">
+                </label>
+            </a>
+
+        </form>
+
+    </div>
+</div>
+
+</body>
+
+</html>
