1ed9f144a0e017257a9134f371b3603e77717d70
Aurelia <aurelia.foucher@outlook.com>
2023-05-15 12:33:18 +0200

    commit views

---
+++ Utils/footer.php
@@ -0,0 +1,16 @@
+   <!-- FOOTER -->
+
+   <footer>
+            <div><a href="">
+                <div class=""><img src="images/circle.png" alt=""></div>
+                <p>Accueil</p></a>
+            </div>
+            <div>
+                <div class=""><img src="images/polygon.png" alt=""></div>
+                <p> </p>
+            </div>
+            <div><a href="">
+                <div class=""><img src="images/star.png" alt=""></div>
+                <p>Déconnexion</p></a>
+            </div>
+        </footer>
---
+++ Views/view_home.php
@@ -0,0 +1,29 @@
+<!-- MAIN -->
+
+<main>
+            <h1>Welcome</h1>
+
+            <div> <a href="pageHtml.html">
+                    <div class="categories"><img src="images/star.png" alt=""></div>
+                    <p>HTML</p>
+                </a>
+            </div>
+
+            <div><a href="pageHtml.html">
+                    <div class="categories"><img src="images/square.png" alt=""></div>
+                    <p>CSS</p>
+                </a>
+            </div>
+            <div><a href="pageHtml.html">
+                    <div class="categories"><img src="images/polygon.png" alt=""></div>
+                    <p>JS</p>
+                </a>
+            </div>
+            <div><a href="pageHtml.html">
+                    <div class="categories"><img src="images/circle.png" alt=""></div>
+                    <p>PHP</p>
+                </a>
+            </div>
+            <div>
+            </div>
+        </main>
