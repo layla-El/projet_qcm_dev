2bcb20269cb00e1e24b40132f29d391680b321ed
Aurelia <aurelia.foucher@outlook.com>
2023-05-16 09:41:14 +0200

    last changes

--- Utils/footer.php
+++ Utils/footer.php
@@ -2,15 +2,15 @@
 
    <footer>
             <div><a href="">
-                <div class=""><img src="images/circle.png" alt=""></div>
+                <div class=""><img src="../Content/circle.png" alt=""></div>
                 <p>Accueil</p></a>
             </div>
-            <div>
-                <div class=""><img src="images/polygon.png" alt=""></div>
-                <p> </p>
+            <div><a href="">
+                <div class=""><img src="../Content/polygon.png" alt=""></div>
+                <p>Profil</p></a>
             </div>
             <div><a href="">
-                <div class=""><img src="images/star.png" alt=""></div>
+                <div class=""><img src="../Content/star.png" alt=""></div>
                 <p>Déconnexion</p></a>
             </div>
         </footer>
--- Views/view_home.php
+++ Views/view_home.php
@@ -1,86 +1,38 @@
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Quizz Dev</title>
-    <link rel="stylesheet" href="css/style.css">
-    <script src="js/app.js" defer></script>
-</head>
-
-<body>
-
-    <div class="container">
+<!-- MAIN -->
 
 
-        <!-- HEADER -->
+<div class="container">
+    
+    <main id="mainIndex">
+        <h1>Welcome</h1>
 
-        <header>
-            <div>
-            </div>
-        </header>
+        <!-- comment mettre ça en hover -->
+        <div> <a href="pageHtml.html">
+                <div class="categories"><img src="../Content/star.png" alt=""></div>
+                <p>HTML</p>
+            </a>
+        </div>
 
-
-
-        <!-- MAIN -->
-
-        <main id="mainIndex">
-            <h1>Welcome</h1>
-
-            <!-- comment mettre ça en hover -->
-            <div> <a href="pageHtml.html">
-                    <div class="categories"><img src="images/star.png" alt=""></div>
-                    <p>HTML</p>
-                </a>
-            </div>
-
-            <div><a href="pageHtml.html">
-                    <div class="categories"><img src="images/square.png" alt=""></div>
-                    <p>CSS</p>
-                </a>
-            </div>
-            <div><a href="pageHtml.html">
-                    <div class="categories"><img src="images/polygon.png" alt=""></div>
-                    <p>JS</p>
-                </a>
-            </div>
-            <div><a href="pageHtml.html">
-                    <div class="categories"><img src="images/circle.png" alt=""></div>
-                    <p>PHP</p>
-                </a>
-            </div>
+        <div><a href="pageHtml.html">
+                <div class="categories"><img src="../Content/square.png" alt=""></div>
+                <p>CSS</p>
+            </a>
+        </div>
+        <div><a href="pageHtml.html">
+                <div class="categories"><img src="../Content/polygon.png" alt=""></div>
+                <p>JS</p>
+            </a>
+        </div>
+        <div><a href="pageHtml.html">
+                <div class="categories"><img src="../Content/circle.png" alt=""></div>
+                <p>PHP</p>
+            </a>
+        </div>
 
         <!-- DIV TRICHE -->
-            <div></div>
-            <div></div>
+        <div></div>
+        <div></div>
 
-        </main>
+    </main>
 
-
-
-        <!-- FOOTER -->
-
-        <footer>
-            <div><a href="">
-                    <div class=""><img src="images/circle.png" alt=""></div>
-                    <p>Accueil</p>
-                </a>
-            </div>
-            <div>
-                <div class=""><img src="images/polygon.png" alt=""></div>
-                <p> </p>
-            </div>
-            <div><a href="">
-                    <div class=""><img src="images/star.png" alt=""></div>
-                    <p>Déconnexion</p>
-                </a>
-            </div>
-        </footer>
-
-    </div>
-
-</body>
-
-</html>
+</div>
