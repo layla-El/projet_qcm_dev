5ab6657de662773fd84c291fa68c18ad650da445
ramdani-nourcine <ncrmd13@gmail.com>
2023-05-16 10:13:40 +0200

    maj

--- Controllers/Controller_reponse.php
+++ Controllers/Controller_reponse.php
@@ -2,16 +2,16 @@
 
 class Controller_reponse extends Controller
 {
-	public function action_default()
-	{
-		$this->action_reponse();
-	}
-	
+    public function action_default()
+    {
+        $this->action_reponse();
+    }
+    
     public function action_all_reponses()
     {
-$m=Model::get_model();
-$data=["reponses"=>$m->get_all_reponses()];
-$this->render("all_reponses",$data);
-
- }
+        $m = Model::get_model();
+        $data = ["reponses" => $m->get_all_reponses()];
+        $this->render("all_reponses", $data);
+    }
 }
+?>
--- Views/View_niveau_questions.php
+++ Views/View_niveau_questions.php
@@ -1,5 +1,25 @@
-<?php foreach ($data["niveaux"] as $n) : ?>
-                <tr>
-                    <td> <?= $n->niveau ?></td>
-                </tr>
-            <?php endforeach; ?>
+     <!-- MAIN -->
+
+     <div class="container">
+
+         <main id="mainNiveaux">
+             <h1>Titre du langage...</h1>
+
+             <div> <a href="">
+                     <div class="categories"><img src="images/star.png" alt="image white star"></div>
+                     <p>Débutant</p>
+                 </a>
+             </div>
+             <div><a href="">
+                     <div class="categories"><img src="images/square.png" alt="image white square"></div>
+                     <p>Intermédiaire</p>
+                 </a>
+             </div>
+             <div><a href="">
+                     <div class="categories"><img src="images/polygon.png" alt="image white polygon"></div>
+                     <p>Avancé</p>
+                 </a>
+             </div>
+
+         </main>
+     </div>
