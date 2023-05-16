0b298ce064f74760f5def113ea1e7502a1e854c0
ramdani-nourcine <ncrmd13@gmail.com>
2023-05-16 11:09:56 +0200

    new commit

--- Views/View_niveau_questions.php
+++ Views/View_niveau_questions.php
@@ -1,5 +1,28 @@
-<?php foreach ($data["niveaux"] as $n) : ?>
+<!-- <?php foreach ($data["niveaux"] as $n) : ?>
                 <tr>
                     <td> <?= $n->niveau ?></td>
                 </tr>
-            <?php endforeach; ?>
+            <?php endforeach; ?> -->
+            <div class="container">
+
+<main id="mainNiveaux">
+    <h1>Titre du langage...</h1>
+
+    <div> <a href="">
+            <div class="categories"><img src="Content/star.png" alt="image white star"></div>
+            <p>Débutant</p>
+        </a>
+    </div>
+    <div><a href="">
+            <div class="categories"><img src="Content/square.png" alt="image white square"></div>
+            <p>Intermédiaire</p>
+        </a>
+    </div>
+    <div><a href="">
+            <div class="categories"><img src="Content/polygon.png" alt="image white polygon"></div>
+            <p>Avancé</p>
+        </a>
+    </div>
+
+</main>
+</div>
