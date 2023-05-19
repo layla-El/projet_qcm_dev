683d4dde32f49bd4e7c977ff5e5061baed3660fe
ramdani-nourcine <ncrmd13@gmail.com>
2023-05-19 11:51:51 +0200

    dossier user ajouté

---
+++ User/Utils/footer.php
@@ -0,0 +1,16 @@
+   <!-- FOOTER -->
+   <link rel="stylesheet" type="text/css" href="Content/style.css">
+   <footer>
+            <div><a href="">
+                <div class=""><img src="Content/images/circle.png" alt="circle"></div>
+                <p>Profil</p></a>
+            </div>
+            <div><a href="?controller=home&action=home?>">
+                <div class=""><img src="Content/images/star.png" alt="star"></div>
+                <p>Accueil</p></a>
+            </div>
+            <div><a href="">
+                <div class=""><img src="Content/images/polygon.png" alt="polygon"></div>
+                <p>Déconnexion</p></a>
+            </div>
+        </footer>
