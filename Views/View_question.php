dbd986b0cde0028edce0c378997220ac6e4dc083
ramdani-nourcine <ncrmd13@gmail.com>
2023-05-19 10:58:48 +0200

    correction erreur view question

--- Views/View_question.php
+++ Views/View_question.php
@@ -13,10 +13,10 @@
 
  <!-- Formulaire pour passer à la question suivante -->
         <form action="?controller=question&action=question&id_theme=<?= $id_theme ?>&niveau=<?= $niveau ?>&question=<?= $questionIndex + 1 ?>"  id="formChoix">
+        <?php $id_question = $currentQuestion->id_question; ?>
+            <?php foreach ($reponses[$id_question] as $reponse) : ?>
             <div class="choix">
  <!-- Récupération des réponses correspondantes à la question affichée -->
-            <?php $id_question = $currentQuestion->id_question; ?>
-            <?php foreach ($reponses[$id_question] as $reponse) : ?>
             <p><?= htmlentities($reponse->libelle_reponse) ?></p>
             </div>
             <?php endforeach; ?>
